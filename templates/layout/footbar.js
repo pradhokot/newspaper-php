37daae820f0d563630627cd7c4e2368a879a60c0
pradhokot <yoggi60@gmail.com>
2025-05-17 22:27:56 +0700

    update footbar collapse menu on mobile

--- templates/layout/footbar.js
+++ templates/layout/footbar.js
@@ -3,26 +3,26 @@ document.writeln(`
    <div class="container">
       <div class="row">
          <div class="col col-12">
-            <div class="row row-cols-1 row-cols-md-auto justify-content-md-between py-4 row-gap-4">
+            <div class="row row-cols-1 row-cols-md-auto justify-content-md-between" style="padding: 2rem 0; row-gap: 2rem;">
                <div class="col">
-                  <h5 class="fs-4 m-0 text-center text-lg-start">Newspaper</h5>
+                  <h5 class="fs-2 m-0 text-center text-lg-start">Newspaper</h5>
                </div><!-- end col -->
                <div class="col">
                   <nav class="nav-sosmed d-flex flex-wrap gap-3 column-gap-4 justify-content-center justify-content-lg-end">
                      <a href="#" target="_blank" class="text-decoration-none text-reset">
-                        <i class="fab fa-x-twitter fa-lg"></i>
+                        <i class="fab fa-x-twitter fa-xl"></i>
                      </a>
                      <a href="#" target="_blank" class="text-decoration-none text-reset">
-                        <i class="fab fa-facebook-f fa-lg"></i>
+                        <i class="fab fa-facebook-f fa-xl"></i>
                      </a>
                      <a href="#" target="_blank" class="text-decoration-none text-reset">
-                        <i class="fab fa-instagram fa-lg"></i>
+                        <i class="fab fa-instagram fa-xl"></i>
                      </a>
                      <a href="#" target="_blank" class="text-decoration-none text-reset">
-                        <i class="fab fa-youtube fa-lg"></i>
+                        <i class="fab fa-youtube fa-xl"></i>
                      </a>
                      <a href="#" target="_blank" class="text-decoration-none text-reset">
-                        <i class="fas fa-rss-square fa-lg"></i>
+                        <i class="fas fa-rss fa-xl"></i>
                      </a>
                   </nav>
                </div><!-- end col -->
@@ -32,11 +32,11 @@ document.writeln(`
          <div class="col col-12">
             <div class="row row-cols-1 row-cols-md-auto justify-content-md-between" id="footbar-menu">
                <div class="col">
-                  <h5 class="text-reset text-center text-md-start my-3 mt-md-4 mb-md-2" data-bs-toggle="collapse" data-bs-target="#footcol1">
+                  <h5 class="footbar-title text-reset my-3 mt-md-4 mb-md-2 collapsed" data-bs-toggle="collapse" data-bs-target="#footcol1">
                      <b>OUR SITES</b>
                   </h5>
                   <div class="collapse d-md-block" id="footcol1" data-bs-parent="#footbar-menu">
-                     <ul class="list-unstyled text-center text-md-start d-flex flex-column row-gap-2 mb-md-5">
+                     <ul class="list-unstyled d-flex flex-column row-gap-2 mb-md-5">
                         <li><a href="" class="link-hover-underline">contact & support</a></li>
                         <li><a href="" class="link-hover-underline">advertise with us</a></li>
                         <li><a href="" class="link-hover-underline">contact & support</a></li>
@@ -52,11 +52,11 @@ document.writeln(`
                </div><!-- end col -->
                <div class="col accordiond-md-none"><hr class="m-0"></div><!-- end col -->
                <div class="col">
-                  <h5 class="text-reset text-center text-md-start my-3 mt-md-4 mb-md-2" data-bs-toggle="collapse" data-bs-target="#footcol2">
+                  <h5 class="footbar-title text-reset my-3 mt-md-4 mb-md-2 collapsed" data-bs-toggle="collapse" data-bs-target="#footcol2">
                      <b>OUR SITES</b>
                   </h5>
                   <div class="collapse d-md-block" id="footcol2" data-bs-parent="#footbar-menu">
-                     <ul class="list-unstyled text-center text-md-start d-flex flex-column row-gap-2 mb-md-5">
+                     <ul class="list-unstyled d-flex flex-column row-gap-2 mb-md-5">
                         <li><a href="" class="link-hover-underline">contact & support</a></li>
                         <li><a href="" class="link-hover-underline">advertise with us</a></li>
                         <li><a href="" class="link-hover-underline">contact & support</a></li>
@@ -72,11 +72,11 @@ document.writeln(`
                </div><!-- end col -->
                <div class="col accordiond-md-none"><hr class="m-0"></div><!-- end col -->
                <div class="col">
-                  <h5 class="text-reset text-center text-md-start my-3 mt-md-4 mb-md-2" data-bs-toggle="collapse" data-bs-target="#footcol3">
+                  <h5 class="footbar-title text-reset my-3 mt-md-4 mb-md-2 collapsed" data-bs-toggle="collapse" data-bs-target="#footcol3">
                      <b>OUR SITES</b>
                   </h5>
                   <div class="collapse d-md-block" id="footcol3" data-bs-parent="#footbar-menu">
-                     <ul class="list-unstyled text-center text-md-start d-flex flex-column row-gap-2 mb-md-5">
+                     <ul class="list-unstyled d-flex flex-column row-gap-2 mb-md-5">
                         <li><a href="" class="link-hover-underline">contact & support</a></li>
                         <li><a href="" class="link-hover-underline">advertise with us</a></li>
                         <li><a href="" class="link-hover-underline">contact & support</a></li>
@@ -92,31 +92,11 @@ document.writeln(`
                </div><!-- end col -->
                <div class="col accordiond-md-none"><hr class="m-0"></div><!-- end col -->
                <div class="col">
-                  <h5 class="text-reset text-center text-md-start my-3 mt-md-4 mb-md-2" data-bs-toggle="collapse" data-bs-target="#footcol3">
-                     <b>OUR SITES</b>
-                  </h5>
-                  <div class="collapse d-md-block" id="footcol3" data-bs-parent="#footbar-menu">
-                     <ul class="list-unstyled text-center text-md-start d-flex flex-column row-gap-2 mb-md-5">
-                        <li><a href="" class="link-hover-underline">contact & support</a></li>
-                        <li><a href="" class="link-hover-underline">advertise with us</a></li>
-                        <li><a href="" class="link-hover-underline">contact & support</a></li>
-                        <li><a href="" class="link-hover-underline">advertise with us</a></li>
-                        <li><a href="" class="link-hover-underline">contact & support</a></li>
-                        <li><a href="" class="link-hover-underline">advertise with us</a></li>
-                        <li><a href="" class="link-hover-underline">contact & support</a></li>
-                        <li><a href="" class="link-hover-underline">advertise with us</a></li>
-                        <li><a href="" class="link-hover-underline">contact & support</a></li>
-                        <li><a href="" class="link-hover-underline">advertise with us</a></li>
-                     </ul>
-                  </div>
-               </div><!-- end col -->
-               <div class="col accordiond-md-none"><hr class="m-0"></div><!-- end col -->
-               <div class="col">
-                  <h5 class="text-reset text-center text-md-start my-3 mt-md-4 mb-md-2" data-bs-toggle="collapse" data-bs-target="#footcol4">
+                  <h5 class="footbar-title text-reset my-3 mt-md-4 mb-md-2 collapsed" data-bs-toggle="collapse" data-bs-target="#footcol4">
                      <b>OUR SITES</b>
                   </h5>	
                   <div class="collapse d-md-block" id="footcol4" data-bs-parent="#footbar-menu">
-                     <ul class="list-unstyled text-center text-md-start d-flex flex-column row-gap-2 mb-md-5">
+                     <ul class="list-unstyled d-flex flex-column row-gap-2 mb-md-5">
                         <li><a href="" class="link-hover-underline">contact & support</a></li>
                         <li><a href="" class="link-hover-underline">advertise with us</a></li>
                         <li><a href="" class="link-hover-underline">contact & support</a></li>
@@ -134,8 +114,13 @@ document.writeln(`
          </div><!-- end col -->
          <div class="col col-12"><hr class="m-0"></div><!-- end col -->
          <div class="col col-12">
-            <div class="row py-4 row-gap-4">
-               <div class="col col-12 col-lg order-lg-2">
+            <div class="row" style="padding: 2rem 0 3rem; row-gap: 2rem;">
+               <div class="col col-12 col-lg-auto">
+                  <p class="text-center text-lg-start mb-0">
+                     &copy; <script>document.write(new Date().getFullYear())</script> Newspaper. All rights reserved.
+                  </p>
+               </div><!-- end col -->
+               <div class="col col-12 col-lg">
                   <nav class="nav-privacy">
                      <a href="#" target="_blank">privacy policy</a>
                      <a href="#" target="_blank">terms of use</a>
@@ -143,11 +128,6 @@ document.writeln(`
                      <a href="#" target="_blank">shipping</a>
                      <a href="#" target="_blank">faq</a>
                   </nav>
-               </div><!-- end col -->
-               <div class="col col-12 col-lg-auto order-lg-1">
-                  <p class="text-center text-lg-start">
-                     &copy; <script>document.write(new Date().getFullYear())</script> Newspaper. All rights reserved.
-                  </p>
                </div><!-- end col -->
             </div><!-- end row -->
          </div><!-- end col -->
