45fa5bc798a894cd2b7a215f3263a73837b1ff49
pradhokot <99374274+pradhokot@users.noreply.github.com>
2025-05-20 21:07:10 +0700

    custom elipsis

--- templates/template2/layout/running-text.js
+++ templates/template2/layout/running-text.js
@@ -4,7 +4,7 @@ document.writeln(`
    <div class="border-bottom border-3 border-danger">
       <div class="text-bg-warning py-1 position-relative" style="height: 2rem;">
          <marquee behavior="" direction="" class="position-absolute top-50 translate-middle-y start-0 end-0">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum soluta iusto fuga alias.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae voluptas, laboriosam soluta quod repudiandae quia illum aperiam? Doloribus ratione distinctio asperiores modi excepturi ipsa? Saepe, commodi! Necessitatibus, in perferendis.
          </marquee>
       </div>
    </div>
