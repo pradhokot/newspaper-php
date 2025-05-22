e477435ce5aef93c55d73b918ec3033983d7bcaf
pradhokot <yoggi60@gmail.com>
2025-05-22 09:25:25 +0700

    2025 mei 22 update 1

--- templates/template2/layout/running-text.js
+++ templates/template2/layout/running-text.js
@@ -1,11 +1,18 @@
 document.writeln(`
 <!-- ----------- running text -->
-<div class="running-text border-top border-3 border-danger">
-   <div class="border-bottom border-3 border-danger">
-      <div class="text-bg-warning py-1 position-relative" style="height: 2rem;">
-         <marquee behavior="" direction="" class="position-absolute top-50 translate-middle-y start-0 end-0">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae voluptas, laboriosam soluta quod repudiandae quia illum aperiam? Doloribus ratione distinctio asperiores modi excepturi ipsa? Saepe, commodi! Necessitatibus, in perferendis.
-         </marquee>
-      </div>
-   </div>
+<div class="border-bottom border-2 border-danger">
+   <div class="row g-0">
+      <div class="col col-auto">
+         <button class="btn btn-primary rounded-0">Leave Testimonial</button>
+      </div><!-- end col -->
+      <div class="col">
+         <div class="h-100 bg-danger w-100">
+            <div class="text-bg-warning py-1 position-relative h-100">
+               <marquee behavior="" direction="" class="position-absolute top-50 translate-middle-y start-0 end-0">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti labore tempora sapiente. Asperiores in ducimus omnis veritatis modi labore facilis minima aperiam odio, fugiat maxime dicta, eligendi alias necessitatibus sed.
+               </marquee>
+            </div>
+         </div>
+      </div><!-- end col -->
+   </div><!-- end row -->
 </div><!-- end running text -->`)
