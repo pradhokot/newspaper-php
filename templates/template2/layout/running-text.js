0a8ea209479891264771fe76148d399791ba55c1
pradhokot <yoggi60@gmail.com>
2025-05-23 09:10:31 +0700

    2025 mei 23 update 1

--- templates/template2/layout/running-text.js
+++ templates/template2/layout/running-text.js
@@ -1,18 +1,21 @@
 document.writeln(`
 <!-- ----------- running text -->
-<div class="border-bottom border-2 border-danger">
-   <div class="row g-0">
-      <div class="col col-auto">
-         <button class="btn btn-primary rounded-0">Leave Testimonial</button>
-      </div><!-- end col -->
-      <div class="col">
-         <div class="h-100 bg-danger w-100">
-            <div class="text-bg-warning py-1 position-relative h-100">
-               <marquee behavior="" direction="" class="position-absolute top-50 translate-middle-y start-0 end-0">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti labore tempora sapiente. Asperiores in ducimus omnis veritatis modi labore facilis minima aperiam odio, fugiat maxime dicta, eligendi alias necessitatibus sed.
-               </marquee>
+<div class="text-bg-warning">
+   <div class="container-xxl">
+      <div class="row g-0 mx-auto">
+         <div class="col col-auto">
+            <button class="btn btn-primary rounded-0">Leave Testimonial</button>
+         </div><!-- end col -->
+         <div class="col">
+            <div class="h-100 w-100">
+               <div class="py-1 position-relative h-100">
+                  <marquee behavior="" direction="" class="position-absolute top-50 translate-middle-y start-0 end-0">
+                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti labore tempora sapiente. Asperiores in ducimus omnis veritatis modi labore facilis minima aperiam odio, fugiat maxime dicta, eligendi alias necessitatibus sed.
+                  </marquee>
+               </div>
             </div>
-         </div>
-      </div><!-- end col -->
-   </div><!-- end row -->
-</div><!-- end running text -->`)
+         </div><!-- end col -->
+      </div><!-- end row -->
+   </div><!-- end container -->
+</div><!-- end running text -->
+`)
