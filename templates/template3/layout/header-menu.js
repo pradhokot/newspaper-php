65c1ee8683585a8bab542ffbaf375680253ef75e
pradhokot <99374274+pradhokot@users.noreply.github.com>
2025-05-27 22:33:09 +0700

    2025 mei 27 update 1

--- templates/template3/layout/header-menu.js
+++ templates/template3/layout/header-menu.js
@@ -1,5 +1,5 @@
 document.writeln(`
-<header class="header-menu">
+<div class="header-menu">
    <div class="menu-top bg-secondary">
       <div class="container-lg px-1 px-lg-3">
          <div class="overflow-x-auto">
@@ -64,22 +64,4 @@ document.writeln(`
          </div>
       </div>
    </div><!-- end menu bottom -->
-   <div class="running-text text-bg-warning">
-      <div class="container-lg px-0 px-lg-3">
-         <div class="row flex-nowrap g-0">
-            <div class="col col-auto">
-               <button class="btn btn-primary rounded-0 border-0 shadow-none">
-                  Leave Testimonial
-               </button>
-            </div><!-- end col -->
-            <div class="col">
-               <marquee behavior="" direction="" class="h-100 d-flex align-items-center">
-                  <a href="#" target="_blank" class="text-decoration-none text-reset">
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, sit magni nam esse quas laboriosam consequatur sed aperiam autem eius distinctio voluptas, odit accusantium cum. Non assumenda eos accusamus asperiores.
-                  </a>
-               </marquee>
-            </div><!-- end col -->
-         </div><!-- end row -->
-      </div><!-- end container -->
-   </div><!-- end running text -->
-</header><!-- end header menu -->`)
+</div><!-- end header menu -->`)
---
+++ templates/template3/layout/running-text.js
@@ -0,0 +1,19 @@
+document.writeln(`
+<div class="running-text text-bg-warning">
+   <div class="container-lg px-0 px-lg-3">
+      <div class="row flex-nowrap g-0">
+         <div class="col col-auto">
+            <button class="btn btn-primary rounded-0 border-0 shadow-none">
+               Leave Testimonial
+            </button>
+         </div><!-- end col -->
+         <div class="col">
+            <marquee behavior="" direction="" class="h-100 d-flex align-items-center">
+               <a href="#" target="_blank" class="text-decoration-none text-reset">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, sit magni nam esse quas laboriosam consequatur sed aperiam autem eius distinctio voluptas, odit accusantium cum. Non assumenda eos accusamus asperiores.
+               </a>
+            </marquee>
+         </div><!-- end col -->
+      </div><!-- end row -->
+   </div><!-- end container -->
+</div><!-- end running text -->`)
